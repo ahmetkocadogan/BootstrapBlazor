e2ae561d3b1ec22de6074f158c19200171c0ab27
Argo-Tianyi <argo@163.com>
2022-01-28 14:42:41 +0800

    test: 更新明细行单元测试

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -2,74 +2,61 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using BootstrapBlazor.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace UnitTest.Components;
 
 public class TableTest : BootstrapBlazorTestBase
 {
-    /// <summary>
-    ///
-    /// </summary>
-    public class Foo
+    private IStringLocalizer<Foo> Localizer { get; }
+
+    public TableTest()
     {
-        // 列头信息支持 Display DisplayName 两种标签
+        Localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+    }
 
-        /// <summary>
-        ///
-        /// </summary>
-        [Key]
-        [Display(Name = "主键")]
-        [AutoGenerateColumn(Ignore = true)]
-        public int Id { get; set; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        [Required(ErrorMessage = "{0}不能为空")]
-        [AutoGenerateColumn(Order = 10, Filterable = true, Searchable = true)]
-        [Display(Name = "姓名")]
-        public string? Name { get; set; } 
-        /// <summary>
-        ///
-        /// </summary>
-        [Required(ErrorMessage = "请选择学历")]
-        [Display(Name = "学历")]
-        [AutoGenerateColumn(Order = 60)]
-        public EnumEducation? Education { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public static List<Foo> GenerateFoo( int count = 10) => Enumerable.Range(1, count).Select(i => new Foo()
+    [Fact]
+    public void TablesDetailRow_Ok()
+    {
+        var detailGenerated = false;
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
         {
-            Id = i,
-            Name = "Foo" + i.ToString(),
-            Education = random.Next(1, 100) > 50 ? EnumEducation.Primary : EnumEducation.Middel
-        }).ToList();
-        private static readonly Random random = new();
+            pb.AddChildContent<Table<Foo>>(pb =>
+            {
+                pb.Add(a => a.OnQueryAsync, OnQueryAsync);
+                pb.Add(a => a.RenderMode, TableRenderMode.Table);
+                pb.Add(a => a.AutoGenerateColumns, true);
+                pb.Add(a => a.DetailRowTemplate, item => builder =>
+                {
+                    detailGenerated = true;
+                    builder.OpenComponent(0, typeof(Table<DetailRow>));
+                    builder.AddAttribute(1, nameof(Table<DetailRow>.Items), DetailRow.GenerateDetailRow(item.Id));
+                    builder.AddAttribute(2, nameof(Table<DetailRow>.AutoGenerateColumns), true);
+                    builder.CloseComponent();
+                });
+            });
+        });
+        var table = cut.FindComponent<Table<Foo>>();
+        var arrow = table.Find(".fa-caret-right");
+        arrow.Click();
 
+        // 断言明细行已生成
+        Assert.True(detailGenerated);
+
+        Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
+        {
+            var items = Foo.GenerateFoo(Localizer, 2);
+            return Task.FromResult(new QueryData<Foo>()
+            {
+                TotalCount = items.Count,
+                Items = items
+            });
+        }
     }
 
-    /// <summary>
-    ///
-    /// </summary>
-    public enum EnumEducation
-    {
-        /// <summary>
-        ///
-        /// </summary>
-        [Display(Name = "小学")]
-        Primary,
-
-        /// <summary>
-        ///
-        /// </summary>
-        [Display(Name = "中学")]
-        Middel
-    }
     private class DetailRow
     {
         /// <summary>
@@ -92,7 +79,6 @@ public class TableTest : BootstrapBlazorTestBase
         [AutoGenerateColumn(Order = 20, Width = 180)]
         public DateTime DateTime { get; set; }
 
-
         /// <summary>
         /// 
         /// </summary>
@@ -101,81 +87,7 @@ public class TableTest : BootstrapBlazorTestBase
         {
             Id = i,
             Name = "DetailRow" + i.ToString(),
-            DateTime=DateTime.Now.AddDays(-count)
+            DateTime = DateTime.Now.AddDays(-count)
         }).ToList();
-
     }
-
-    private List<Foo>? Items { get; set; } = Foo.GenerateFoo();
-    private List<DetailRow>? DetailRows(int i) => DetailRow.GenerateDetailRow(i);
-    private bool flag;
-        // 异步点击
-        TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
-
-    [Fact]
-    public async void TablesDetailRow_Ok()
-    {
-        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
-        {
-            pb.AddChildContent(TablesDetailRow());
-        });
-        var table = cut.FindComponent<Table<Foo>>();
-        await table.InvokeAsync(async () => { await table.Instance.QueryAsync(); tcs.SetResult(true);});
-        await tcs.Task;
-        var html = table.Markup;
-
-
-        //因为懒加载 loading...无法测试 , :-<
-
-        //var cut = Context.RenderComponent<Table<Foo>>(builder => {
-        //    builder.Add(a => a.AutoGenerateColumns, true);
-        //    builder.Add(a => a.Items, Items);
-        //    builder.Add(a => a.IsDetails, _=true);
-        //    builder.Add(a => a.OnAfterRenderCallback, OnAfterRenderCallback());
-        //    builder.Add(a => a.DetailRowTemplate, GenerateDetailRow());
-        //    }
-        //);
-        //cut.InvokeAsync(() => cut.Instance.OnAfterRenderCallback());
-        Assert.Contains("class=\"form-control checkbox-list\"", cut.Markup);
-    }
-    public RenderFragment TablesDetailRow() => builder =>
-    {
-        builder.OpenComponent(0, typeof(Table<Foo>));
-        builder.AddAttribute(1, nameof(Table<Foo>.OnQueryAsync),((QueryPageOptions options) => OnQueryAsync(options)));
-        //builder.AddAttribute(1, nameof(Table<Foo>.Items), Items);
-        builder.AddAttribute(2, nameof(Table<Foo>.IsDetails), _ = true);
-        builder.AddAttribute(3, nameof(Table<Foo>.OnAfterRenderCallback), OnAfterRenderCallback());
-        builder.AddAttribute(4, nameof(Table<Foo>.DetailRowTemplate), GenerateDetailRow());
-        builder.AddAttribute(5, nameof(Table<Foo>.AutoGenerateColumns), true);
-        builder.CloseComponent();
-    };
-
-
-    Func<Table<Foo>, Task>? OnAfterRenderCallback()
-    {
-        return  _ => Task.CompletedTask;
-    }
-    public Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
-    {
-        flag = !flag;
-        return Task.FromResult( new QueryData<Foo>()
-        {
-            TotalCount = flag?0: Items?.Count??0,
-            Items = flag ? null : Items
-        }) ;
-    }
-
-
-    private RenderFragment<Foo> GenerateDetailRow() 
-    {
-        return  new RenderFragment<Foo>(item => builder =>
-        {
-            builder.OpenComponent(0, typeof(Table<DetailRow>));
-            builder.AddAttribute(1, nameof(Table<DetailRow>.Items), DetailRows(item.Id));
-            builder.AddAttribute(2, nameof(Table<DetailRow>.AutoGenerateColumns), true);
-            builder.CloseComponent();
-        }); 
-    }
-
-
 }
