9cf24513a3f29cc3713972d6a141e037793346b7
Argo-Lenovo <argo@163.com>
2022-02-16 01:29:55 +0800

    test: 增加辅助方法

--- test/UnitTest/Components/TableFilterTest.cs
+++ test/UnitTest/Components/TableFilterTest.cs
@@ -11,4 +11,48 @@ namespace UnitTest.Components;
 
 public class TableFilterTest : BootstrapBlazorTestBase
 {
+    private static RenderFragment<Foo> CreateTableColumns() => foo => builder =>
+    {
+        var index = 0;
+        builder.OpenComponent<TableColumn<Foo, string>>(index++);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, string>.Field), foo.Name);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, string>.FieldExpression), foo.GenerateValueExpression());
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, string>.Filterable), true);
+        builder.CloseComponent();
+
+        builder.OpenComponent<TableColumn<Foo, bool>>(index++);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, bool>.Field), foo.Complete);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, bool>.FieldExpression), foo.GenerateValueExpression(nameof(Foo.Complete), typeof(bool)));
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, bool>.Filterable), true);
+        builder.CloseComponent();
+
+        builder.OpenComponent<TableColumn<Foo, int>>(index++);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, int>.Field), foo.Count);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, int>.FieldExpression), foo.GenerateValueExpression(nameof(Foo.Count), typeof(int)));
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, int>.Filterable), true);
+        builder.CloseComponent();
+
+        builder.OpenComponent<TableColumn<Foo, EnumEducation?>>(index++);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, EnumEducation?>.Field), foo.Education);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, EnumEducation?>.FieldExpression), foo.GenerateValueExpression(nameof(Foo.Education), typeof(EnumEducation?)));
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, EnumEducation?>.Filterable), true);
+        builder.CloseComponent();
+
+        builder.OpenComponent<TableColumn<Foo, DateTime?>>(index++);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, DateTime?>.Field), foo.DateTime);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, DateTime?>.FieldExpression), foo.GenerateValueExpression(nameof(Foo.DateTime), typeof(DateTime?)));
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, DateTime?>.Filterable), true);
+        builder.CloseComponent();
+
+        builder.OpenComponent<TableColumn<Foo, IEnumerable<string>>>(index++);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, IEnumerable<string>>.Field), foo.Hobby);
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, IEnumerable<string>>.Lookup), new List<SelectedItem>()
+        {
+            new SelectedItem("1", "Test1"),
+            new SelectedItem("2", "Test2"),
+        });
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, IEnumerable<string>>.FieldExpression), foo.GenerateValueExpression(nameof(Foo.Hobby), typeof(IEnumerable<string>)));
+        builder.AddAttribute(index++, nameof(TableColumn<Foo, IEnumerable<string>>.Filterable), true);
+        builder.CloseComponent();
+    };
 }
