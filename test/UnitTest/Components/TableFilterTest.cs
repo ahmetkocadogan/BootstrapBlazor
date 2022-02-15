36982749365b23e362e2a4f9d94ddd0ae6b0d1ed
Argo-Lenovo <argo@163.com>
2022-02-16 01:29:35 +0800

    test: 增加 Filter 单元测试

---
+++ test/UnitTest/Components/TableFilterTest.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using UnitTest.Extensions;
+
+namespace UnitTest.Components;
+
+public class TableFilterTest : BootstrapBlazorTestBase
+{
+}
