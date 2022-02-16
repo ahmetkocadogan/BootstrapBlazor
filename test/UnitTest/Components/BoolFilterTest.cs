9129799411d49d8076f786353d3ef901820b5888
Argo-Lenovo <argo@163.com>
2022-02-16 10:28:04 +0800

    test: 增加 BoolFilter 单元测试

---
+++ test/UnitTest/Components/BoolFilterTest.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class BoolFilterTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Reset_Ok()
+    {
+        var cut = Context.RenderComponent<BoolFilter>();
+
+        var filter = cut.Instance;
+        cut.InvokeAsync(() => filter.Reset());
+    }
+    [Fact]
+    public void GetFilterConditions_Ok()
+    {
+        var cut = Context.RenderComponent<BoolFilter>();
+
+        var filter = cut.Instance;
+        IEnumerable<FilterKeyValueAction>? condtions = null;
+        cut.InvokeAsync(() => condtions = filter.GetFilterConditions());
+        Assert.Empty(condtions);
+
+        // Set Value
+        var items = cut.FindAll(".dropdown-item");
+        cut.InvokeAsync(() => items[1].Click());
+        cut.InvokeAsync(() => condtions = filter.GetFilterConditions());
+        Assert.Single(condtions);
+    }
+}
