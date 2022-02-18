92b4c2d5c870fd42b191b3dfbd806876e2ae6798
Argo-Tianyi <argo@163.com>
2022-02-18 12:18:54 +0800

    doc: 格式化文档

--- src/BootstrapBlazor.Shared/Components/TreeDataFoo.cs
+++ src/BootstrapBlazor.Shared/Components/TreeDataFoo.cs
@@ -25,27 +25,27 @@ class TreeDataFoo
     public static List<TreeItem> GetTreeItems()
     {
         var items = new List<TreeDataFoo>
-            {
-                new TreeDataFoo() { Text = "导航一", Code = "1010" },
-                new TreeDataFoo() { Text = "导航二", Code = "1020" },
-                new TreeDataFoo() { Text = "导航三", Code = "1030" },
+        {
+            new TreeDataFoo() { Text = "导航一", Code = "1010" },
+            new TreeDataFoo() { Text = "导航二", Code = "1020" },
+            new TreeDataFoo() { Text = "导航三", Code = "1030" },
 
-                new TreeDataFoo() { Text = "子菜单一", Code = "1040", ParentCode = "1020" },
-                new TreeDataFoo() { Text = "子菜单二", Code = "1050", ParentCode = "1020" },
-                new TreeDataFoo() { Text = "子菜单三", Code = "1060", ParentCode = "1020" },
+            new TreeDataFoo() { Text = "子菜单一", Code = "1040", ParentCode = "1020" },
+            new TreeDataFoo() { Text = "子菜单二", Code = "1050", ParentCode = "1020" },
+            new TreeDataFoo() { Text = "子菜单三", Code = "1060", ParentCode = "1020" },
 
-                new TreeDataFoo() { Text = "孙菜单一", Code = "1070", ParentCode = "1050" },
-                new TreeDataFoo() { Text = "孙菜单二", Code = "1080", ParentCode = "1050" },
-                new TreeDataFoo() { Text = "孙菜单三", Code = "1090", ParentCode = "1050" },
+            new TreeDataFoo() { Text = "孙菜单一", Code = "1070", ParentCode = "1050" },
+            new TreeDataFoo() { Text = "孙菜单二", Code = "1080", ParentCode = "1050" },
+            new TreeDataFoo() { Text = "孙菜单三", Code = "1090", ParentCode = "1050" },
 
-                new TreeDataFoo() { Text = "曾孙菜单一", Code = "1100", ParentCode = "1080" },
-                new TreeDataFoo() { Text = "曾孙菜单二", Code = "1110", ParentCode = "1080" },
-                new TreeDataFoo() { Text = "曾孙菜单三", Code = "1120", ParentCode = "1080" },
+            new TreeDataFoo() { Text = "曾孙菜单一", Code = "1100", ParentCode = "1080" },
+            new TreeDataFoo() { Text = "曾孙菜单二", Code = "1110", ParentCode = "1080" },
+            new TreeDataFoo() { Text = "曾孙菜单三", Code = "1120", ParentCode = "1080" },
 
-                new TreeDataFoo() { Text = "曾曾孙菜单一", Code = "1130", ParentCode = "1100" },
-                new TreeDataFoo() { Text = "曾曾孙菜单二", Code = "1140", ParentCode = "1100" },
-                new TreeDataFoo() { Text = "曾曾孙菜单三", Code = "1150", ParentCode = "1100" }
-            };
+            new TreeDataFoo() { Text = "曾曾孙菜单一", Code = "1130", ParentCode = "1100" },
+            new TreeDataFoo() { Text = "曾曾孙菜单二", Code = "1140", ParentCode = "1100" },
+            new TreeDataFoo() { Text = "曾曾孙菜单三", Code = "1150", ParentCode = "1100" }
+        };
 
         // 算法获取属性结构数据
         return GetSubItems(null, items).ToList();
