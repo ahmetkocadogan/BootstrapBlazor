e80ff4c656b0288bb74854b2052355d48236e232
Argo-Tianyi <argo@163.com>
2022-02-18 12:19:38 +0800

    test: 增加 Tree 单元测试

---
+++ test/UnitTest/Components/TreeTest.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class TreeTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Items_Ok()
+    {
+        var cut = Context.RenderComponent<Tree>();
+        cut.Contains("tree-root");
+
+        // 由于 Items 为空不生成 TreeItem
+        cut.DoesNotContain("li");
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Items, new List<TreeItem>()
+            {
+                new TreeItem() { Text = "Test1", id },
+                new TreeItem() { Text = "Test2", IsActive = true }
+            });
+        });
+    }
+}
